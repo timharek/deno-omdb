c8f0b4fd5e4e2d779f332019303063184a3d5006
Tim Hårek Andreassen <tim@harek.no>
2022-07-19 23:14:18 +0200

    feat: Install cliffy
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,11 +1,3 @@
-export { parse } from 'https://deno.land/std@0.145.0/flags/mod.ts';
-export type { Args } from 'https://deno.land/std@0.145.0/flags/mod.ts';
-export { printHelp } from 'https://deno.land/x/deno_cli_help@1.0.0/mod.ts';
-export type { Options } from 'https://deno.land/x/deno_cli_help@1.0.0/mod.ts';
-
-export const options = {
-  api: '',
-  format: 'long',
-};
+export { Command } from 'https://deno.land/x/cliffy@v0.24.2/command/mod.ts';
 
 export const REQUEST_URL = new URL('https://www.omdbapi.com/');
