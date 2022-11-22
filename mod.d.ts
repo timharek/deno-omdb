d6869ec7a44b9342eba436e9fad44ed5c0f3f39d
Tim Hårek Andreassen <tim@harek.no>
2022-11-22 23:19:04 +0100

    feat: Add types
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

---
+++ mod.d.ts
@@ -0,0 +1,77 @@
+interface Query {
+  apiKey: string;
+  id?: string;
+  title?: string;
+  verbose: number;
+}
+
+interface Result {
+  title: string
+  year: number
+  released: string
+  runtime: string | null
+  genre: string[]
+  director: string[] | null
+  writer: string[]
+  actors: string[]
+  plot: string
+  language: string
+  country: string
+  awards: string
+  poster: string
+  ratings: Rating[],
+  metascore: string
+  imdbRating: string | null
+  imdbVotes: string | null
+  imdbID: string
+  imdbUrl: string
+  type: "movie" | "series"
+  dvd: string
+  boxOffice: string | null
+  totalSeasons: number | null
+  production: string
+  website: string
+  response: string
+}
+
+interface Rating {
+  source: string
+  value: string
+}
+
+declare namespace OMDb {
+  interface Response {
+    Title: string
+    Year: string
+    Rated: string
+    Released: string
+    Runtime: string
+    Genre: string
+    Director: string
+    Writer: string
+    Actors: string
+    Plot: string
+    Language: string
+    Country: string
+    Awards: string
+    Poster: string
+    Ratings: Rating[]
+    Metascore: string
+    imdbRating: string
+    imdbVotes: string
+    imdbID: string
+    Type: "movie" | "series"
+    DVD?: string
+    TotalSeasons?: string
+    BoxOffice: string
+    Production: string
+    Website: string
+    Response: string
+  }
+
+  interface Rating {
+    Source: string
+    Value: string
+  }
+}
+
