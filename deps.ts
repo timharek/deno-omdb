c36a04c8c92f6439e5483cec257b56d5f403c094
Tim Hårek Andreassen <tim@harek.no>
2022-11-22 22:42:48 +0100

    chore(deps): Update
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { Command } from 'https://deno.land/x/cliffy@v0.24.2/command/mod.ts';
+export { Command } from 'https://deno.land/x/cliffy@v0.25.4/command/mod.ts';
 
 export const REQUEST_URL = new URL('https://www.omdbapi.com/');
