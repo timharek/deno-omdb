d98bffbc46f2d1b10c9df85fea73a621b0959c3b
Tim Hårek Andreassen <tim@harek.no>
2023-11-18 18:13:19 +0100

    chore(deps): Update cliffy
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,6 +1,2 @@
-export { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts';
-export {
-  GithubProvider,
-  UpgradeCommand,
-} from 'https://deno.land/x/cliffy@v0.25.7/command/upgrade/mod.ts';
+export { Command } from 'https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts';
 export { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts';
